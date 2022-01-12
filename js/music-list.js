08a1f967cf9d7f817154f886a9a31ed3e6066b3b
AJITH KUMAR M <89124776+ajitharunai@users.noreply.github.com>
2022-01-12 07:20:51 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "8  Things can be change my life",
+    artist: "Ajith Kumar M ",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Thirukkural",
+    artist: "Ajith Kumar M ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Honest Gift",
+    artist: "Ajith Kumar M",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Motivation Story",
+    artist: "Ajith Kumar M ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Motivation Story",
+    artist: "Ajith Kumar M ",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Motivation Story",
+    artist: "Ajith Kumar M ",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
